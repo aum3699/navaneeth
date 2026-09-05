932850566a7ec7a7683ee9b41b1d7e3a941fac26
aum3699 <aum3699@example.com>
2026-09-05 19:53:54 +0530

    Fix vite config for Netlify build

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
-import { defineConfig } from "@tanstack/react-start/vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  vite: {
-    plugins: [
-      react(),
-      tailwindcss(),
-      tsConfigPaths({
-        root: "./",
-      }),
-    ],
-  },
+  plugins: [
+    react(),
+    tailwindcss(),
+    tsConfigPaths({
+      root: "./",
+    }),
+  ],
 });
